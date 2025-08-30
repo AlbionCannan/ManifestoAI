93e4cf25a9793234ef588c7d0813077c871b41ac
AlbionCannan <tommy@aoun.net>
2025-08-30 15:05:40 +0100

    Update assumptions.js

--- config/assumptions.js
+++ config/assumptions.js
@@ -1,1 +1,19 @@
+export const DEFAULT_ASSUMPTIONS = {
+  general: {
+    vat_spend_share: 0.70,       // share of income exposed to VAT
+    fuel_liters_per_month: 70    // default car fuel usage per month
+  },
+  byCountry: {
+    France: {},
+    Germany: {},
+    Italy: {},
+    Poland: {},
+    Spain: {}
+  }
+};
 
+export function getAssumption(country, key){
+  const map = DEFAULT_ASSUMPTIONS.byCountry[country] || {};
+  if (key in map) return map[key];
+  return DEFAULT_ASSUMPTIONS.general[key];
+}
