63c67ea1e5dafd201ed0da5b94090c78f3d674c7
AlbionCannan <tommy@aoun.net>
2025-09-01 20:15:09 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import "./countryPortals.css"; // optional / safe
+import "./countryPortals.css"; // optional
 import App from "./App";
 
 createRoot(document.getElementById("root")).render(<App />);
